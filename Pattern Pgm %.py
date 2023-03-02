1349e9b5745bbc0331f9aefae12be7edee078ab3
RSK2004 <126455002+RSK2004@users.noreply.github.com>
2023-03-02 14:52:26 +0530

    Add files via upload

---
+++ Pattern Pgm %.py
@@ -0,0 +1,6 @@
+n = int(input("Enter number of rows:"))
+print(" ")
+for i in range(1,n):
+    for j in range(1):
+        print("%"*i)
+        print(" ")
---
+++ Square and Cube of a decimal number.py
@@ -0,0 +1,5 @@
+n = float(input("Enter a decimal number:"))
+sq = n**2
+cube = n**3
+print("Square of",n,"=",sq)
+print("Cube of",n,"=",cube)
